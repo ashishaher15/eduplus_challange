8abd5176f5c2a1df40492a3d894506d136ab419c
sunil5151 <sunil.22210652@viit.ac.in>
2025-07-03 17:24:46 +0530

    f

--- app-backend/server.js
+++ app-backend/server.js
@@ -13,7 +13,7 @@ import { initDatabase } from './src/controllers/initController.js';
 
 const app = express();
 app.use(cors({
-  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173', 'https://demo-1-rjfr.onrender.com'],
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:5173', 'https://demo-1-pgla.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
